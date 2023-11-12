9596187327d6e7670d45bafdfcdd93efb07c2dcd
sean <seankh4444@gmail.com>
2023-11-12 13:12:12 -0800

    hot fix filePath

--- library_management_system/src/App.java
+++ library_management_system/src/App.java
@@ -7,7 +7,7 @@ import view.Index;
 public class App {
     public static void main(String[] args) throws Exception {
         try {
-            String filePath = "CS151_Library_Management_System\\library_management_system\\database\\books.json";
+            String filePath = "library_management_system/database/books.json";
             List<Book> books = Book.readFromJsonFile(filePath);
             // only displaying the first book
             if (!books.isEmpty()) {
