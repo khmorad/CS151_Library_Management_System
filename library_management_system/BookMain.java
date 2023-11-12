98921f8e23cb1344cc4c95041442ce9a2e4ab932
khmoorad <khakho.morad@gmail.com>
2023-11-12 00:09:35 -0800

    fixed bugs

--- library_management_system/BookMain.java
+++ library_management_system/BookMain.java
@@ -8,7 +8,6 @@ public class BookMain {
         try {
             String filePath = "CS151_Library_Management_System\\library_management_system\\database\\books.json";
             List<Book> books = Book.readFromJsonFile(filePath);
-
             // Display the information of the first book in the original JSON file
             if (!books.isEmpty()) {
                 books.get(0).displayInfo();
